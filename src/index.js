e511aa4994c2e058f4455ddfe52a5afa1fa0155c
unknown <oscar.1995.roca@gmail.com>
2018-08-10 17:16:39 -0400

    add title

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Paper from '@material-ui/core/Paper'
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
-class App extends Component {
-    render() {
-        return(
-            <h1>Hola Papu</h1>
-        )
+const styles = (theme) => {
+    return{
+            container: {
+            width: '500px',
+            margin: '0 auto'
+            }
     }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+class App extends Component {
+  render() {
+    const classes = this.props.classes
+    return (
+      <Paper className={classes.container}>
+        <Typography 
+        variant="display1" 
+        color="secondary"
+        >
+          Registor de la Comunidad
+        </Typography>
+      </Paper>
+    );
+  }
+}
+
+App = withStyles(styles)(App)
+
+ReactDOM.render(<App />, document.getElementById("root"));
