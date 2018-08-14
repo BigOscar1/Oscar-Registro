563f1b44bd39f3b2fc4c34211392ba13d20ab1dd
unknown <oscar.1995.roca@gmail.com>
2018-08-14 17:49:49 -0400

    Languaje Configuration

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom"
 import { withStyles } from "@material-ui/core/styles"
 import MomentUtils from "material-ui-pickers/utils/moment-utils"
 import MuiPickersUtilsProvider from "material-ui-pickers/utils/MuiPickersUtilsProvider"
+
+import moment from "moment"
+import "moment/locale/es"
+
 //Los de Abajo son secundarios
-import styles from './styles'
 import Form from './Form'
+import styles from './styles'
+
+moment.locale('es') //moment parsea lenguajes
 
 class App extends Component {
   render() {
     return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
+    <MuiPickersUtilsProvider utils={MomentUtils} locale='es' moment={moment}>
         <Form classes={this.props.classes}/>
       </MuiPickersUtilsProvider>
     );
